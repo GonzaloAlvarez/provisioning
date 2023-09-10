04018ac494385283098307d7c86b6fcfff134e42
Gonzalo Alvarez <gonzaloab@gmail.com>
2023-09-10 16:57:59 -0700

    Updated and simplified bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
-mkdir -p "$HOME/.bootstrap"
-cd "$HOME/.bootstrap"
-wget --quiet "https://bootstrap.pypa.io/virtualenv.pyz"
-if [ $(type -P "python3") ]; then
-    PYTHON=python3
-elif [ $(type -P "python") ]; then
-    PYTHON=python
-else
-    echo "python not found in path. exiting"
-    exit 1
-fi
-$PYTHON virtualenv.pyz venv
+mkdir -p "$HOME/.bootstrap" && cd "$_"
+$(command -v wget) --quiet "https://bootstrap.pypa.io/virtualenv.pyz"
+$(command -v python3 || command -v python) virtualenv.pyz venv || exit 1
 source venv/bin/activate
-pip install --upgrade pip setuptools wheel
-pip install ansible
-if [ -d "$HOME/provisioning" ]; then
-    cp -Rv "$HOME/provisioning" .
-else
-    pip install dulwich --config-settings "--build-option=--pure"
-    $PYTHON - <<'____HERE'
-from dulwich import porcelain
-porcelain.clone('https://github.com/GonzaloAlvarez/provisioning', 'provisioning')
-____HERE
-fi
-cd provisioning
+python -m pip install --upgrade pip setuptools wheel
+[[ -d "$HOME/provisioning" ]] || python -m  install dulwich --config-settings "--build-option=--pure"
+[[ -d "$HOME/provisioning" ]] || python -c "from dulwich import porcelain;porcelain.clone('https://github.com/GonzaloAlvarez/provisioning', 'provisioning')"
+cp -Rv "$HOME/provisioning" . && cd provisioning
+python -m pip install ansible
 ansible-galaxy install -r requirements.yml
 ansible-playbook --connection=local --inventory=localhost --limit 127.0.0.1 playbooks/main.yml
