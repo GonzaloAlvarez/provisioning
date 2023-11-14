0a9131c956b24a69d8b4c4635e79b18a0a1e5673
Gonzalo Alvarez <gonzaloab@gmail.com>
2023-11-13 17:45:57 -0800

    Fixed bootstrap.sh to properly install dulwich

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ $(command -v wget) --quiet "https://bootstrap.pypa.io/virtualenv.pyz"
 $(command -v python3 || command -v python) virtualenv.pyz venv || exit 1
 source venv/bin/activate
 python -m pip install --upgrade pip setuptools wheel
-[[ -d "$HOME/provisioning" ]] || python -m  install dulwich --config-settings "--build-option=--pure"
+[[ -d "$HOME/provisioning" ]] || python -m pip install dulwich --config-settings "--build-option=--pure"
 [[ -d "$HOME/provisioning" ]] || python -c "from dulwich import porcelain;porcelain.clone('https://github.com/GonzaloAlvarez/provisioning', 'provisioning')"
 cp -Rv "$HOME/provisioning" . && cd provisioning
 python -m pip install ansible
