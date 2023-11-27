3f4d415f428b7cc59bd52f373bb85d9cd897ab2d
Gonzalo Alvarez <gonzaloab@gmail.com>
2023-11-26 18:50:28 -0800

    Updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 mkdir -p "$HOME/.bootstrap" && cd "$_"
-$(command -v wget) --quiet "https://bootstrap.pypa.io/virtualenv.pyz"
+$(command -v wget || command -v curl) -o virtualenv.pyz "https://bootstrap.pypa.io/virtualenv.pyz" || exit 1
 $(command -v python3 || command -v python) virtualenv.pyz venv || exit 1
 source venv/bin/activate
 python -m pip install --upgrade pip setuptools wheel
-[[ -d "$HOME/provisioning" ]] || python -m pip install dulwich --config-settings "--build-option=--pure"
-[[ -d "$HOME/provisioning" ]] || python -c "from dulwich import porcelain;porcelain.clone('https://github.com/GonzaloAlvarez/provisioning', 'provisioning')"
-cp -Rv "$HOME/provisioning" .; cd provisioning
+python -m  install dulwich --config-settings "--build-option=--pure" && python -c "from dulwich import porcelain;porcelain.clone('https://github.com/GonzaloAlvarez/provisioning', 'provisioning')"
+[[ -d "$HOME/provisioning" ]] && cp -Rf "$HOME/provisioning" .
+cd provisioning
 python -m pip install ansible
 ansible-galaxy install -r requirements.yml
 ansible-playbook --connection=local --inventory=localhost --limit 127.0.0.1 playbooks/main.yml
